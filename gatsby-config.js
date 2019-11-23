200ecc4e6c423e105e854a5cd492b8524ef06db6
Alsu <alsu.96@hotmail.fi>
2019-11-23 14:47:59 +0200

    Change site config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Hämpton Games`,
+    description: `Hämpton Games on pelitapahtuma joka järjestetään Hämeenlinnassa 11.1.2020`,
+    author: `Bunkkeri`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
