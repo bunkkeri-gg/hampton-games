eca984c2c71762da39354454e330785a1fa5b0e3
Alsu <alsu.96@hotmail.fi>
2020-01-08 12:38:11 +0200

    Timetable styling

--- src/components/timetable.js
+++ src/components/timetable.js
@@ -11,19 +11,19 @@ const TimeTable = () => (
           <h2>Super Smash Bros. Ultimate</h2>
           <table>
             <tr>
-              <td>11:00</td>
+              <td style={{ width: "30%" }}>11:00</td>
               <td>Aalto A</td>
             </tr>
             <tr>
-              <td>12:30</td>
+              <td style={{ width: "30%" }}>12:30</td>
               <td>Aalto B</td>
             </tr>
             <tr>
-              <td>14:30</td>
+              <td style={{ width: "30%" }}>14:30</td>
               <td>Top 8</td>
             </tr>
             <tr>
-              <td>18:00</td>
+              <td style={{ width: "30%" }}>18:00</td>
               <td>Finaalit</td>
             </tr>
           </table>
@@ -32,35 +32,38 @@ const TimeTable = () => (
           <h2>Counter-Strike: Global Offensive</h2>
           <table>
             <tr>
-              <td>11:00</td>
+              <td style={{ width: "30%" }}>11:00</td>
               <td>Joukkue 1 - Joukkue 2 (Vanajasali)</td>
             </tr>
             <tr>
-              <td>11:00</td>
+              <td style={{ width: "30%" }}>11:00</td>
               <td>Joukkue 3 - Joukkue 4 (Yläkerta)</td>
             </tr>
             <tr>
-              <td>12:30</td>
+              <td style={{ width: "30%" }}>12:30</td>
               <td>Joukkue 5 - Joukkue 6 (Vanajasali)</td>
             </tr>
             <tr>
-              <td>12:30</td>
+              <td style={{ width: "30%" }}>12:30</td>
               <td>Joukkue 7 - Joukkue 8 (Yläkerta)</td>
             </tr>
             <tr>
-              <td>13:30</td>
+              <td style={{ width: "30%" }}>13:30</td>
               <td>Voittaja 1 - Voittaja 2 (Vanajasali)</td>
             </tr>
             <tr>
-              <td>14:30</td>
+              <td style={{ width: "30%" }}>14:30</td>
               <td>Voittaja 3 - Voittaja 4 (Vanajasali)</td>
             </tr>
             <tr>
-              <td>15:30</td>
+              <td style={{ width: "30%" }}>15:30</td>
               <td>Finaali (Paras kolmesta)</td>
             </tr>
           </table>
         </div>
+        <p style={{ color: "red", fontWeight: "500" }}>
+          Aikatauluihin voi tulla muutoksia.
+        </p>
       </div>
     </div>
   </div>
