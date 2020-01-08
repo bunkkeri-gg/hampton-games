48a16e2a96b5bfde41866d5bd0b7cfe5aaa32ba9
Alsu <alsu.96@hotmail.fi>
2020-01-08 12:37:51 +0200

    Maps styling

--- src/components/maps.js
+++ src/components/maps.js
@@ -14,29 +14,57 @@ const Maps = () => (
       <h1 style={{ margin: 0 }}>Reitit</h1>
     </div>
     <div style={{ marginLeft: "2em", marginRight: "2em" }}>
-      <h2>Julkinen liikenne</h2>
+      <h2>Tapahtumapaikka</h2>
       <div
         style={{
           marginBottom: "1rem",
           paddingLeft: "1rem",
         }}
       >
-        <a href="https://goo.gl/maps/zQPHvFBtjSpXcYAaA" target="__blank">
-          Hämeenlinnan Juna-asema
+        <a href="https://goo.gl/maps/HMvdHFy6GkVAsMse9" target="__blank">
+          Verkatehdas
         </a>
-        <a href="https://goo.gl/maps/y3XS6LSfqf3GAkxW7" target="__blank">
-          Hämeenlinnan linja-autoasema
-        </a>
-        <h3>Lähimmät bussipysäkit</h3>
-        <a href="https://goo.gl/maps/Em8J3whcXbqqUTMQ9" target="__blank">
-          Pikkujärvi E
-        </a>
-        <a href="https://goo.gl/maps/SWytMkqsbcGb29wM8" target="__blank">
-          Terveyskeskus I
-        </a>
-        <a href="https://goo.gl/maps/crCvjZ3Ngzzb1Ns49" target="__blank">
-          Pikkujärvi P
-        </a>
+      </div>
+      <h2>Julkinen liikenne</h2>
+      <div>
+        <div
+          style={{
+            marginBottom: "1rem",
+            paddingLeft: "1rem",
+          }}
+        >
+          <a href="https://goo.gl/maps/zQPHvFBtjSpXcYAaA" target="__blank">
+            Hämeenlinnan Juna-asema
+          </a>
+          <a href="https://goo.gl/maps/y3XS6LSfqf3GAkxW7" target="__blank">
+            Hämeenlinnan linja-autoasema
+          </a>
+          <div
+            style={{
+              marginTop: "1rem",
+              marginBottom: "1rem",
+            }}
+          >
+            <h3>Lähimmät bussipysäkit</h3>
+            <div
+              style={{
+                marginTop: "1rem",
+                marginBottom: "1rem",
+                paddingLeft: "1rem",
+              }}
+            >
+              <a href="https://goo.gl/maps/Em8J3whcXbqqUTMQ9" target="__blank">
+                Pikkujärvi E
+              </a>
+              <a href="https://goo.gl/maps/SWytMkqsbcGb29wM8" target="__blank">
+                Terveyskeskus I
+              </a>
+              <a href="https://goo.gl/maps/crCvjZ3Ngzzb1Ns49" target="__blank">
+                Pikkujärvi P
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
       <h2>Kaupat</h2>
       <div
