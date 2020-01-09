16de1783059d270ff0b9cc8ef46359698a1000e5
Alsu <alsu.96@hotmail.fi>
2020-01-09 17:32:42 +0200

    Add vanaja sponsor logo

--- src/components/sponsor.js
+++ src/components/sponsor.js
@@ -4,6 +4,7 @@ import hml from "../images/sponsors/hml.png"
 import showtekniikka from "../images/sponsors/showtekniikka.png"
 import sporttiveikot from "../images/sponsors/sporttiveikot.png"
 import verkatehdas from "../images/sponsors/verkatehdas.png"
+import vanaja from "../images/sponsors/vanaja.png"
 
 const Sponsor = () => (
   <div>
@@ -18,11 +19,12 @@ const Sponsor = () => (
       <h1 style={{ margin: 0 }}>Yhteistyössä</h1>
     </div>
     <div style={{ marginLeft: "2em", marginRight: "2em", display: "flex", flexWrap: "wrap", justifyContent: "space-evenly", alignItems: "center" }}>
-      <img src={verkatehdas} alt="Verkatehdas" style={{width: "301px", height: "29px"}}/>
-      <img src={hml} alt="Hämeenlinna" style={{width: "118px", height: "118px"}}/>
-      <img src={hamk} alt="HAMK" style={{width: "200px", height: "60px"}}/>
-      <img src={showtekniikka} alt="ShowTekniikka" style={{width: "177px", height: "51px"}}/>
-      <img src={sporttiveikot} alt="SporttiVeikot" style={{width: "248px", height: "37px"}}/>
+      <img src={verkatehdas} alt="Verkatehdas" style={{width: "301px", height: "29px", margin: "10px"}}/>
+      <img src={hml} alt="Hämeenlinna" style={{width: "118px", height: "118px", margin: "10px"}}/>
+      <img src={vanaja} alt="Hämeenlinna-Vanaja Seurakunta" style={{width: "263px", height: "99px", margin: "10px"}}/>
+      <img src={hamk} alt="HAMK" style={{width: "200px", height: "60px", margin: "10px"}}/>
+      <img src={showtekniikka} alt="ShowTekniikka" style={{width: "177px", height: "51px", margin: "10px"}}/>
+      <img src={sporttiveikot} alt="SporttiVeikot" style={{width: "248px", height: "37px", margin: "10px"}}/>
     </div>
   </div>
 )
